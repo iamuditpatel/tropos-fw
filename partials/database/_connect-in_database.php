956e0c6909592874a14cfc5db9e4af1ffb198453
Udit Patel <uditatulpatel@gmail.com>
2021-01-15 13:19:40 +0530

    added database connection

---
+++ partials/database/_connect-in_database.php
@@ -0,0 +1,14 @@
+<?php
+  $server = "localhost";
+  $username = "root";
+  $password = "";
+  $database = "hackos";
+
+  $connect = mysqli_connect($server, $username, $password, $database);
+  if (!$connect){
+  //  echo "success";
+  //}
+  //else {
+    die("Error". mysqli_connect_error());
+  }
+  ?>

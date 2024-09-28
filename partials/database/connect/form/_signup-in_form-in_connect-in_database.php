0dc7d4e0705b13e0f2aa0928a231bf08f7a6d719
Udit Patel <admin@uditpatel.com>
2024-09-28 16:47:06 +0530

    Update to warzone 0000 _signup-in_form-in_connect-in_database.php
    added database signup module

--- partials/database/connect/form/_signup-in_form-in_connect-in_database.php
+++ partials/database/connect/form/_signup-in_form-in_connect-in_database.php
@@ -2,24 +2,29 @@
 $show_alert = false;
 $show_error = false;
 
-  if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+    { 
+            include 'partials/database/connect/_connect-in_database.php';
 
-    include 'partials/database/connect/_connect-in_database.php';
+            $username = $_POST["username"];
+            $password = $_POST["password"];
+            $confirm_password = $_POST["confirm-password"];
+            $exists = false;
 
-    $username = $_POST["username"];
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-    $confirm_password = $_POST["confirm-password"];
-    $exists = false;
-    if(($password == $confirm_password) && $exists == false){
-      $sql = "INSERT INTO `users` (`username`, `email`, `password`, `date_time`) VALUES ('$username', '$email', '$password', current_timestamp())";
-      $result = mysqli_query($connect, $sql);
-      if ($result){
-        $show_alert = true;
-      }
+                        if(($password == $confirm_password) && $exists == false)
+                        {
+                                $sql = "INSERT INTO `login` (`username`,`password`) VALUES ('$username','$password')";
+                                $result = mysqli_query($connect, $sql);
+
+                                        if ($result)
+                                        {
+                                                $show_alert = true;
+                                        }
+                        }
+        else
+        {
+                $show_error = "Password do not match";
+        }
+  
     }
-    else{
-      $show_error = "Password do not match";
-    }
-  }
 ?>

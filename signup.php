9653670de9aae4ce6d9cf819c61e7071fe18e904
Udit Patel <admin@uditpatel.com>
2024-09-28 16:33:39 +0530

    Update tp warzone 0000 signup.php
    added signup form module with database signup form module connected with main database module

--- signup.php
+++ signup.php
@@ -1,42 +1,16 @@
-  <?php
-    require 'partials/database/connect/form/_signup-in_form-in_connect-in_database.php';
-  ?>
+<?php require 'partials/database/connect/_connect-in_database.php'; ?>
+<?php require 'partials/database/connect/form/_signup-in_form-in_connect-in_database.php'; ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-
 <head>
-
-  <?php
-    require 'partials/head/meta/_meta-in_head.php';
-  ?>
-
-  <?php
-    require 'partials/head/css/_css-in_head.php';
-  ?>
-
-  <title>Hackosphere.Co</title>
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup</title>
 </head>
-
 <body>
-
-  <?php
-    require 'partials/body/nav/_nav-in_body.php';
-  ?>
-
-  <?php
-    require 'partials/body/alert/_signup-in_alert-in_body.php';
-  ?>
-
-  <?php
-    require 'partials/body/form/_signup-in_form-in_body.php';
-  ?>
-
-  <?php
-    require 'partials/body/js/_js-in_body.php';
-  ?>
-
+    <?php require 'partials/body/nav/_nav-in_body.php'; ?>
+    <h1>Signup</h1>
+    <?php require 'partials/body/form/_signup-in_form-in_body.php'; ?>
 </body>
-
 </html>

542f30386d32486a0cf27eb13a1a792fbea0e898
Udit Patel <admin@uditpatel.com>
2024-09-28 17:02:27 +0530

    Update to warzone 0000 _signin-in_form-in_body.php
    added signup form module in body

--- partials/body/form/_signin-in_form-in_body.php
+++ partials/body/form/_signin-in_form-in_body.php
@@ -1,28 +1,18 @@
-<div class="container">
-  <h1 class="text-center">Signin to our website</h1>
-
-<!-- _Signin in _Form in _Body start here -->
-<form>
-
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-
-  <button type="submit" class="btn btn-primary">Submit</button>
-
+<form action="signin.php" method="post">
+    <input type="text" id="username" name="username" placeholder="Username" required><br>
+    <input type="password" id="password" name="password" placeholder="Password" required><br>
+    <input type="submit" id="btn" name="submit" value="Signin">
 </form>
-<!-- _Signin in _Form in _Body start here -->
 
-</div>
+<?php if(isset($error)): ?>
+    <p><?php echo $error; ?></p>
+    <?php endif; ?>
+
+    <?php
+    // Check if user is already logged in
+    if(isset($_SESSION['username'])) {
+        // Redirect to dashboard
+        header("Location: dashboard.php");
+        exit();
+    }
+?>
